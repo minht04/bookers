1afe8c35a56008c640c21fbc5b704c35feaee2ae
ochiaihitomi <sky.force.1013@gmail.com>
2021-01-12 08:40:29 +0000

    [Update] 3-10

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,5 +1,16 @@
 class TodolistsController < ApplicationController
   def new
-  @book = Book.new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/index'
+  end
+  
+  private
+  def book_params
+    params.permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'top' => 'books#top'
   root to: 'books#top'
   
-  get '/index.html' => 'books#index'
   
-  get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
   
-  post 'todolists' => 'todolists#create'
   
 end
