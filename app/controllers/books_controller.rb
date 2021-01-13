b4c73bcdeca47c5ddfadced2a5cc3ec5d2249ce8
ochiaihitomi <sky.force.1013@gmail.com>
2021-01-13 06:44:51 +0000

    [Update] 3-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,12 +29,14 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to books_path
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
