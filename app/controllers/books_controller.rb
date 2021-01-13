ad24f564df3f902a31d0d95cac49b9bbf653dee1
ochiaihitomi <sky.force.1013@gmail.com>
2021-01-13 05:54:52 +0000

    [Update] 3-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-      redirect_to book_path(book)
+      redirect_to books_path
   end
 
   def index
@@ -26,10 +26,18 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
+  
+  
 
   private
   def book_params
